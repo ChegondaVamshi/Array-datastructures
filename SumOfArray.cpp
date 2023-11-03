751d85df9a414dd64b968b2c1dc61f9f98b3fbd5
ChegondaVamshi <149768302+ChegondaVamshi@users.noreply.github.com>
2023-11-03 23:18:13 +0530

    SumOfArrayElements.cpp

--- SumOfArray.cpp
+++ SumOfArray.cpp
@@ -3,35 +3,35 @@ using namespace std;
 int main() {
     int n;
 
-    // Ask the user for the number of elements in the array
-    cout << "Enter the number of elements in the array: ";
+    
+    cout << "Enter the number of elements in the array: "<<"/n";
     cin >> n;
 
-    // Check for invalid input
+    
     if (n <= 0) {
-        cout << "Invalid input. Please enter a positive number of elements." << endl;
+        cout << "Invalid input. Please enter a positive number of elements." << "/n";
         return 1;
     }
 
-    // Declare an array of size 'n'
+    
     int arr[n];
 
-    // Ask the user to enter the elements of the array
-    cout << "Enter " << n << " elements of the array:" << endl;
+
+    cout << "Enter " << n << " elements of the array:" << "/n";
     for (int i = 0; i < n; i++) {
         cin >> arr[i];
     }
 
-    // Initialize a variable to store the sum
+    
     int sum = 0;
 
-    // Calculate the sum of all elements in the array
+    
     for (int i = 0; i < n; i++) {
         sum += arr[i];
     }
 
-    // Display the sum
-    cout << "Sum of all elements in the array: " << sum << endl;
+    
+    cout << "Sum of all elements in the array: " << sum <<"/n";
 
     return 0;
 }
